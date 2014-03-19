f02b6776b9827e3f1ed027ac9d9464f0cf3a1bb0
Maor Hayun <maor.hayun@gmail.com>
2014-03-19 13:00:24 +0200

    readded tests

---
+++ CalcServiceDemo.Tests1/CalcControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalcTestLib;
+using Rhino.Mocks;
+using CalcServiceDemo.Controllers;
+
+namespace CalcServiceDemo.Tests1
+{
+    [TestClass]
+    public class CalcControllerTests
+    {
+        [TestMethod]
+        public void GetAdd_SmokeTest()
+        {
+            // mock utils inteface
+            ICalcUtils utils = MockRepository.GenerateStub<ICalcUtils>();
+            utils.Stub(x => x.Add(2, 2)).Return(4);
+
+            CalcController calc = new CalcController(utils);
+            Assert.AreEqual("result was: 4", calc.GetAdd(2, 2));
+        }
+    }
+}
