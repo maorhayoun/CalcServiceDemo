16f1cec1f004a79d412ba121e6af5a1db12d0ba8
Maor Hayun <maor.hayun@gmail.com>
2014-03-19 13:13:18 +0200

    added manualy test project

---
+++ CalcServiceDemo.Tests2/CalcControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcServiceDemo.Tests2
+{
+    [TestClass]
+    public class CalcControllerTests
+    {
+        [TestMethod]
+        public void GetAdd_SmokeTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
