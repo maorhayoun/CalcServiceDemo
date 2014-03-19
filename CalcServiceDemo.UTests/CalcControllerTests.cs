42597256c329faf9f49aabccbeddbfc5c0dd5dfc
Maor Hayun <maor.hayun@gmail.com>
2014-03-19 13:19:34 +0200

    nunit proj

---
+++ CalcServiceDemo.UTests/CalcControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcServiceDemo.UTests
+{
+    [TestFixture]
+    public class CalcControllerTests
+    {
+        [Test]
+        public void GetAdd_SmokeTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
