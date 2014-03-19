9d4c525f227fcbb028c7599bc1fcda75625571e2
Maor Hayun <maor.hayun@gmail.com>
2014-03-19 10:52:43 +0200

    first commit

---
+++ CalcServiceDemo/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using MyCalcTestLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AttributeRouting.Framework.Localization;
+using AttributeRouting.Web.Constraints;
+using AttributeRouting.Web.Http.WebHost;
+using AttributeRouting.Web.Http.Framework;
+using AttributeRouting.Web;
+using AttributeRouting.Web.Http;
+using AttributeRouting;
+
+namespace CalcServiceDemo.Controllers
+{
+    [RoutePrefix("calc")]
+    public class CalcController : ApiController
+    {
+        private readonly CalcUtils _lib = new CalcUtils();
+        
+        [GET("add/{x}/{y}")]
+        public string GetAdd(int x, int y)
+        {
+            return string.Format("result was: {0}", _lib.Add(x, y));
+        }
+
+        [GET("sub/{x}/{y}")]
+        public string GetSub(int x, int y)
+        {
+            // TODO: implement subract
+            //return string.Format("result was: {0}", _lib.Subtract(x, y));
+            return string.Empty;
+        }
+    }
+}
---
+++ CalcServiceDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CalcServiceDemo.WebApiApplication" Language="C#" %>
