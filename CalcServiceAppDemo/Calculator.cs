418b5d784e042069de56e9e0aa978afd86d1803c
Maor Hayun <maor.hayun@gmail.com>
2014-03-19 13:48:35 +0200

    calculator app service

---
+++ CalcServiceAppDemo/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using MyCalcTestLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcServiceAppDemo
+{
+    public class CalculatorService
+    {
+        ICalcUtils _calcUtils;
+        public CalculatorService()
+            : this (new CalcUtils())
+        {
+        }
+
+        public CalculatorService(ICalcUtils calcUtils)
+        {
+            _calcUtils = calcUtils;
+        }
+
+        public string Calc(string operation, int x, int y)
+        {
+            switch (operation)
+            {
+                case "add": return string.Format("result was: {0}", _calcUtils.Add(x, y));
+                //case "sub": return string.Format("result was: {0}", _calcUtils.Subtract(x, y));
+
+                default:
+                    return "not a valid operation";
+            }
+        }
+    }
+}
---
+++ CalcServiceAppDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcServiceAppDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorService service = new CalculatorService();
+
+            Console.WriteLine("Enter params for calculation in (operation) (x) (y) format:");
+            Console.WriteLine("e.g. add 3 2");
+            string input = Console.ReadLine();
+
+            do
+            {
+                string[] parts = input.Split(' ');
+
+                string result = service.Calc(parts[0], int.Parse(parts[1]), int.Parse(parts[2]));
+                Console.WriteLine(result);
+            }
+            while ((input = Console.ReadLine()) != "");
+        }
+    }
+}
